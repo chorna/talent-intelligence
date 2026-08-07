e1c62208bd8172f6b4f44a9ee216bfb9570da6dd
Christian Horna <christian.horna@qds.com.pe>
2026-08-06 21:58:08 -0500

    feat(core): add core in installed apps

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+BASE_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -45,6 +45,16 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+
+THIRD_APPS = [
+    "rest_framework",
+]
+
+LOCAL_APPS = [
+    "apps.core",
+]
+
+INSTALLED_APPS = BASE_APPS + THIRD_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
